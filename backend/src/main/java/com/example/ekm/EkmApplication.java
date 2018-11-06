10d33559a458be82b4cab73ef1bd70c3a0af48f8
Age Roosi <Age.Roosi@4finance.com>
2018-11-06 13:55:59 +0200

    test

--- backend/src/main/java/com/example/ekm/EkmApplication.java
+++ backend/src/main/java/com/example/ekm/EkmApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EkmApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EkmApplication.class, args);
 	}
