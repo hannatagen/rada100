bfb4921dd21a0340f6449e9b810eabfe6855b334
Age Roosi <Age.Roosi@4finance.com>
2018-11-06 09:32:29 +0200

    Added reposiotry annotation.

--- backend/src/main/java/com/example/ekm/Repository/TrailRepository.java
+++ backend/src/main/java/com/example/ekm/Repository/TrailRepository.java
@@ -2,9 +2,11 @@ package com.example.ekm.Repository;
 
 import com.example.ekm.Model.Trail;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TrailRepository extends CrudRepository<Trail, Long> {
     List<Trail> findAll();
 }
