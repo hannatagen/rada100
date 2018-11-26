bec4b7744348b8039d7c8c41e0fd957674cad630
ageroosi <Age.Roosi@4finance.com>
2018-11-26 22:45:27 +0200

    Added endpoint security.

--- backend/src/main/java/com/example/ekm/Security/SpringSecurityWebAppConfig.java
+++ backend/src/main/java/com/example/ekm/Security/SpringSecurityWebAppConfig.java
@@ -2,6 +2,7 @@ package com.example.ekm.Security;
 
 import com.example.ekm.Repository.GameUserRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,6 +30,17 @@ public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/games/").hasAnyAuthority("PLAYER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/games/point/{point_id}").hasAnyAuthority("PLAYER", "ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/games/point/{point_id}").hasAnyAuthority("PLAYER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/games/{trail_id}").hasAnyAuthority("PLAYER", "ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/games/{trail_id}").hasAnyAuthority("PLAYER", "ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/games/{trail_id}/started").hasAnyAuthority("PLAYER", "ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/games/{trail_id}/ended").hasAnyAuthority("PLAYER", "ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/users/").hasAnyAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/users/information").hasAnyAuthority("PLAYER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/points/").hasAnyAuthority("ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/trails/").hasAnyAuthority("ADMIN")
                 .anyRequest().permitAll()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager(), gameUserRepository))
