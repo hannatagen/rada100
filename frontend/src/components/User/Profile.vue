b675c78c9003f75766551b967c820e1aa787da4c
htagen <hanna.tagen@gmail.com>
2018-11-26 16:33:05 +0200

    point test

--- frontend/src/components/User/Profile.vue
+++ frontend/src/components/User/Profile.vue
@@ -7,15 +7,22 @@
                 <h5 class="card-title">{{ username }}</h5>
             </div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">
-                    <strong>Tase:</strong>
-                    <span id="level">3</span>
-                </li>
+                <!--Jääb praegu skoobist välja-->
+                <!--<li class="list-group-item">-->
+                    <!--<strong>Tase:</strong>-->
+                    <!--<span id="level">3</span>-->
+                <!--</li>-->
                 <li class="list-group-item">
                     <h6 class="card-title">Pooleliolevad mängud:</h6>
                 </li>
                 <li class="list-group-item">
                     <h6 class="card-title">Lõpetatud mängud:</h6>
+                    <div v-if="completedTrailsList.length > 0">
+                        <div v-for="trail in completedTrailsList" :key="trail.trail_id">
+                            {{ trail.name }}
+                        </div>
+                    </div>
+                    <div v-else>Ühtegi mängu pole lõpetatud.</div>
                 </li>
             </ul>
         </div>
@@ -33,12 +40,32 @@
         data() {
             return {
                 username: '',
+                trailsList: [],
+                completedTrailsList: [],
             };
         },
         methods: {
             setUsername(username) {
                 this.username = username;
             },
+            trailsData(trails) {
+                this.trailsList = trails;
+            },
+            completedTrails() {
+                for (const trail in this.trailsList) {
+                    AXIOS.get('/api/games/' + trail.trail_id + '/ended')
+                        .then(response => {
+                            // JSON responses are automatically parsed.
+                            const isCompleted = response.data;
+                            if(isCompleted) {
+                                this.completedTrailsList.push(trail);
+                            }
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        });
+                }
+            }
         },
         mounted() {
             AXIOS.get('/api/users/information', {
@@ -46,14 +73,26 @@
                     Authorization: this.$store.state.loggedInToken,
                     'Content-Type': 'application/json',
                 }
-            }).then(request => {
-                console.log(request.data);
-                const username = request.data.username;
-                console.log(username);
-                this.setUsername(username);
-            }).catch(error => {
-                console.log(error)
-            });
+                }).then(request => {
+                    console.log(request.data);
+                    const username = request.data.username;
+                    console.log(username);
+                    this.setUsername(username);
+                }).catch(error => {
+                    console.log(error)
+                });
+
+            AXIOS.get('/api/trails/')
+                .then(response => {
+                    // JSON responses are automatically parsed.
+                    const trails = response.data;
+                    this.trailsData(trails);
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+
+            this.completedTrails();
         },
     };
 </script>
