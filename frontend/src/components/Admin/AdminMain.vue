8ff4a535ea4dd8d63daf5cc3bcb5b6779ad335da
htagen <hanna.tagen@gmail.com>
2019-03-17 23:12:56 +0200

    admin main greeting

--- frontend/src/components/Admin/AdminMain.vue
+++ frontend/src/components/Admin/AdminMain.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
         <AdminSidebar v-if="$route.name !== 'AdminLogin'"></AdminSidebar>
-        <router-view/>
+        <div id="contentContainer">
+            <div id="greetingDiv" v-if="$route.name === 'AdminMain'">
+                <h1>Tere tulemast!</h1>
+                <h4>Alustamiseks vali vasakult menüüst sobiv tegevus.</h4>
+            </div>
+            <router-view v-else/>
+        </div>
     </div>
 </template>
 
@@ -15,5 +21,23 @@
 </script>
 
 <style scoped>
+    #contentContainer {
+        position: fixed;
+        top: 6em;
+        left: 220px;
+        text-align: center;
+        width: 80vw;
+        height: 100vh;
+    }
 
+    @media only screen and (max-width: 600px) {
+        #contentContainer {
+            top: 1em;
+            left: 70px;
+        }
+    }
+
+    #greetingDiv {
+        margin-top: 25vh;
+    }
 </style>
