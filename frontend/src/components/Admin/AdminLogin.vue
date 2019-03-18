00cdee64a0928e468b6e78b8df6e3a97f1d4b717
htagen <hanna.tagen@gmail.com>
2019-03-18 21:39:44 +0200

    admin login

--- frontend/src/components/Admin/AdminLogin.vue
+++ frontend/src/components/Admin/AdminLogin.vue
@@ -5,28 +5,58 @@
         <h3 id="adminLoginTitle">SadaRada halduskeskkond</h3>
         <div class="container subAdminLoginContainer">
             <div class="form-group">
-                <label for="exampleInputAdminName">Kasutajanimi</label>
-                <input type="text" class="form-control" id="exampleInputAdminName"
-                       placeholder="Sisesta kasutajanimi...">
+                <label for="inputAdminName">Email</label>
+                <input type="text" class="form-control" id="inputAdminName"
+                       placeholder="Sisesta email..."
+                        v-model="email">
             </div>
             <div class="form-group">
-                <label for="exampleInputPassword1">Parool</label>
-                <input type="password" class="form-control" id="exampleInputPassword1"
-                       placeholder="Sisesta parool...">
+                <label for="inputAdminPassword">Parool</label>
+                <input type="password" class="form-control" id="inputAdminPassword"
+                       placeholder="Sisesta parool..."
+                        v-model="password">
             </div>
-            <button @click="adminLogin()" class="btn btnGreen loginAdminButton">Logi sisse</button>
+            <button @click="loginUser()" class="btn btnGreen loginAdminButton">Logi sisse</button>
         </div>
     </div>
 </template>
 
 <script>
+    import { AXIOS } from './.././http-common'
+
     export default {
         name: 'AdminLogin',
+        data() {
+            return {
+                email: '',
+                password: '',
+            };
+        },
         methods: {
-            adminLogin() {
-                this.$store.commit('setAdminTokenID', true);
+            loginUser() {
+                AXIOS.post('/login', {username: this.email, password: this.password}) // TODO username : email
+                    .then(request => {
+                        this.loginSuccessful(request);
+                    })
+                    .catch(error => {
+                        //eslint-disable-next-line
+                        console.log(error);
+                        document.getElementById('inputAdminName').style.border = 'red 2px solid';
+                        document.getElementById('inputAdminPassword').style.border = 'red 2px solid'
+                    })
+            },
+            loginSuccessful(req) {
+                if (!req.headers.authorization) {
+                    this.loginFailed();
+                    return
+                }
+                this.$store.commit('setUserTokenID', req.headers.authorization);
                 this.$router.replace('/admin/main')
-            }
+            },
+            loginFailed () {
+                //eslint-disable-next-line
+                console.log('Login failed!')
+            },
         }
     };
 </script>
